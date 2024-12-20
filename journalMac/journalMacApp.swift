f3306ae88742ef85a2257f142b9ea0b0a5ece45a
cryozeus <yashbhardwaj911@gmail.com>
2024-12-20 23:10:34 +0530

    init commit with xcode setup

---
+++ journalMac/journalMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalMacApp.swift
+//  journalMac
+//
+//  Created by Yash Bhardwaj on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct journalMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ journalMacTests/journalMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  journalMacTests.swift
+//  journalMacTests
+//
+//  Created by Yash Bhardwaj on 20/12/24.
+//
+
+import Testing
+@testable import journalMac
+
+struct journalMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
